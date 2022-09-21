f924416b985fc0f6352438bad62132e96eece1af
volodymyryaniv <erjaniv@ukr.net>
2022-09-21 10:23:30 +0300

    Delete unused code

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -5,14 +5,3 @@ export interface ToDoItemTypes {
   expireUntil: string;
   completed: boolean;
 }
-
-export interface withFormSimplePropTypes {
-  text: string;
-  setText: (value: string) => void;
-  onSubmit: (value: React.SyntheticEvent) => void;
-}
-
-export interface withFormFullPropTypes extends withFormSimplePropTypes {
-  setCreated: (value: string) => void;
-  setExpire: (value: string) => void;
-}
