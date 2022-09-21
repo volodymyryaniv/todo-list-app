22c9f22f8557de72a28f84f2dd686cdda2e61ada
volodymyryaniv <erjaniv@ukr.net>
2022-09-21 12:12:54 +0300

    Added checkbox for todo status

--- client/src/components/ToDoItem/ToDoItem.tsx
+++ client/src/components/ToDoItem/ToDoItem.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
+import { useAppDispatch } from '../../hooks/redux-hooks';
+import { setTodoStatus } from '../../redux/slices/todoListSlice';
 import { ToDoItemTypes } from '../../types';
 import styles from './ToDoItem.module.scss';
 
-const ToDoItem: React.FC<ToDoItemTypes> = ({ text, created, expireUntil }) => {
-  const formatDate = (date: string) => {
+const ToDoItem: React.FC<ToDoItemTypes> = ({
+  id,
+  text,
+  created,
+  expireUntil,
+  completed,
+}) => {
+  const dispatch = useAppDispatch();
+
+  const formatDate = (date: string): string => {
     const arr = date.split('T');
     const newDate = arr[0].split('-').reverse().join('.');
     const newTime = arr.at(-1)?.slice(0, 5);
     return `${newDate} ${newTime}`;
   };
 
+  const formatClassNames = (
+    status: ToDoItemTypes['completed'],
+    name: string
+  ): string => {
+    return status ? `${styles[name]} ${styles.done}` : `${styles[name]}`;
+  };
+
   const formatedCreated = formatDate(created);
   const formatedExpire = formatDate(expireUntil);
+
+  const textStyle = formatClassNames(completed, 'text');
+  const dateStyle = formatClassNames(completed, 'date');
+
+  const onSetStatusHandler = (id: ToDoItemTypes['id']) => {
+    dispatch(setTodoStatus(id));
+  };
 
   return (
     <article className={styles.container}>
       <div className={styles.mainInfo}>
-        <p className={styles.text}>{text}</p>
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          checked={completed}
+          onChange={() => onSetStatusHandler(id)}
+        />
+        <p className={textStyle}>{text}</p>
       </div>
       <div className={styles.details}>
-        <span className={styles.date}>{formatedCreated}</span>
-        <span className={styles.date}>{formatedExpire}</span>
+        <span className={dateStyle}>{formatedCreated}</span>
+        <span className={dateStyle}>{formatedExpire}</span>
       </div>
     </article>
   );
